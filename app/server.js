6a0da12cd5ed2bd73d88ecbe998d43f005ecf457
Jonathan Chung <jonathanchung01@gmail.com>
2023-11-06 21:12:55 -0500

    Signup page and functionality

--- app/server.js
+++ app/server.js
@@ -1,6 +1,7 @@
 let express = require("express");
 let path = require("path");
 let pg = require("pg");
+let bcrypt = require("bcrypt");
 let app = express();
 
 app.use(express.json());
@@ -52,6 +53,53 @@ app.post("/feed", (req, res) => {
       console.log(error);
     });
 });
+
+let saltRounds = 10;
+
+app.post("/signup", (req, res) => {
+    let username = req.body.username;
+    let plaintextPassword = req.body.plaintextPassword;
+    let confirmPassword = req.body.confirmPassword;
+
+    if (!username instanceof String || !plaintextPassword instanceof String) {
+        console.log("not string");
+        res.status(401).send();
+    }
+    else if (username.length < 4 || username.length > 15) {
+        console.log("invalid username length");
+        res.status(401).send();
+    }
+    else if (plaintextPassword.length < 4) {
+        console.log("invalid password length");
+        res.status(401).send();
+    }
+    else if (plaintextPassword != confirmPassword) {
+        console.log("passwords dont match");
+        res.status(401).send();
+    }
+    else {
+        bcrypt
+        .hash(plaintextPassword, saltRounds)
+        .then((hashedPassword) => {
+            pool
+                .query(
+                    "INSERT INTO accountinfo (username, hashedPassword) VALUES ($1, $2)",
+                    [username, hashedPassword],
+                )
+                .then(() => {
+                    console.log(username, "account created");
+                    res.status(200).send();
+                })
+                .catch((error) => {
+                    console.log(error);
+                    res.status(500).send();
+                });
+        }).catch((error) => {
+            console.log(error);
+            res.status(500).send();
+        });
+    }
+})
 
 app.listen(port, hostname, () => {
   console.log(`http://${hostname}:${port}`);
--- setup.sql
+++ setup.sql
@@ -1,6 +1,12 @@
+DROP DATABASE IF EXISTS userdata;
 CREATE DATABASE userdata;
 \c userdata
 CREATE TABLE posts (
 	id SERIAL PRIMARY KEY,
 	post VARCHAR(250)
-)
+);
+CREATE TABLE accountinfo (
+    id SERIAL PRIMARY KEY,
+    username VARCHAR(15),
+    hashedPassword VARCHAR(100)
+);
